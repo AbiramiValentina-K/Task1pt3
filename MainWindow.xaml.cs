959d0795f28d291f7f2e00116b5ceac05b6ac83b
AbiramiValentina-K <abiramivalentina@graspear.com>
2023-03-20 17:56:06 +0530

    Dispatcher Timer

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Task1pt3
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Label.Content = e.Key;
+            Label.Visibility = Visibility.Visible;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            Label.Visibility = Visibility.Collapsed;
+            timer.Stop();
+        }
+    }
+}
